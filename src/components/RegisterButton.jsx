42c4b0a3e53c18ed423b2d9fb60c4e5bdbf6a18e
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2024-02-19 15:55:36 +1000

    Created Register button

---
+++ src/components/RegisterButton.jsx
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function RegisterButton() {
+    return (
+        <>
+            <Button fixed="bottom" href="/login">Register</Button>
+        </>
+    );
+}
+
+export default RegisterButton;
