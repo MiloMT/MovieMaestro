988b51598ebff34b5bb843c6c13e1d9814d587aa
Mfern10 <mitchellfernandez147@gmail.com>
2024-02-19 10:04:34 +0900

    Setup page URL routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import '../styles/App.css'
+import Home from '../views/Home';
+import Login from '../views/Login';
+import Movie from '../views/Movie';
+import Profile from '../views/Profile';
 
 function App() {
 
   return (
-    <>
-      <h1>MovieMaestro!</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/profile" element={<Profile />} />
+        <h1>MovieMaestro!</h1>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
