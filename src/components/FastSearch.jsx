4fcafb618c43752b10a0c447bb408b429d56ac37
Mfern10 <mitchellfernandez147@gmail.com>
2024-02-19 13:17:33 +0900

    Added fast search feature and genre selector feature

---
+++ src/components/FastSearch.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import Accordion from "react-bootstrap/Accordion";
+import GenreSelector from "./GenreSelector"; // Import the GenreSelector component here
+
+import Button from 'react-bootstrap/Button';
+
+function MovieRequest(genre) {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genre}`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhY2NkNDA5NDQ3MGU3YmFhNjZmYTg0MjljYmU3OTM3YiIsInN1YiI6IjY1YzVkODg5OGUyMGM1MDE2NDMzMTM4MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OBGleiES5kWMJQmgbRAvnzsSEGmJvHcx-tBkr454SoY",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data.results));
+}
+
+function FastSearch() {
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  let [genre, setGenre] = useState("")
+
+
+
+  return (
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Fast Search</Accordion.Header>
+        <Accordion.Body>
+          <GenreSelector setGenre={setGenre} />
+          <Button onClick={MovieRequest(genre)} variant="primary">Search Movie</Button>{" "}
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
+
+export default FastSearch;
+
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import FastSearch from '../components/FastSearch';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div>Home</div>
+      <FastSearch />
+    </>
+  );
 }
 
 export default Home
