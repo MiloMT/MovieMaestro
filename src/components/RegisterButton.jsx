0707030ef859d77cdb63b580a5fd6f6b22b5f5f9
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2024-02-19 16:10:20 +1000

    Added text

--- src/components/RegisterButton.jsx
+++ src/components/RegisterButton.jsx
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 function RegisterButton() {
     return (
         <>
+            <div>Want faster searches? Don't want to input your streaming platform availability everytime? Than register for an account to get default settings applied straight away!</div>
             <Button fixed="bottom" href="/login">Register</Button>
         </>
     );
