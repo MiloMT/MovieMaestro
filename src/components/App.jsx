99220ef108328ad2df9e9170dbd57e3fb90b8637
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2024-02-19 14:57:02 +1000

    Created Navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,13 @@ import Home from '../views/Home';
 import Login from '../views/Login';
 import Movie from '../views/Movie';
 import Profile from '../views/Profile';
+import NavBar from './Navbar';
 
 function App() {
 
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import NavBar from '../components/Navbar'
 
 const Home = () => {
   return (
+    <>
     <div>Home</div>
+    </>
+
   )
 }
 
