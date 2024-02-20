85cb6109954acdd59b04fc2b21f20b1b569a7570
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2024-02-20 11:07:52 +1000

    Created register and login page

--- src/components/RegisterLogin.jsx
+++ src/components/RegisterLogin.jsx
@@ -2,28 +2,41 @@ import { useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 function RegisterLogin() {
 
-    const [action, setAction] = useState('Login')
+    const [action, setAction] = useState('Register')
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
+    const handleName = (event) => {
+        console.log(event.target.value)
+        setName(event.target.value)
+    }
+    const handleEmail = (event) => {
+        console.log(event.target.value)
+        setEmail(event.target.value)
+    }
+
 
     return (
         <Form>
             <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction('Register')}}>Register</div>
             <div className={action==="Register"?"submit gray":"submit"} onClick={()=>{setAction('Login')}}>Login</div>
             {action==="Login"?<div></div>:<Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
-        <Form.Control type="name" placeholder="Name" />
+        <Form.Control type="name" value={name} onChange={event => handleName(event)} placeholder="Name" />
             </Form.Group>}
             
             <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-        <Form.Control type="email" placeholder="Email" />
+        <Form.Control type="email" value={email} onChange={event => handleEmail(event)} placeholder="Email" />
             </Form.Group>
             <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
                     <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-            {action==="Register"?<div></div>:<div className="forgot-password">Forgot password?</div>}
+            {action==="Register"?<div></div>:<Button variant="link">Forgot password?</Button>}
+            <div></div>
             {action==="Register"?<button type="submit">Create Account</button>:<button type="submit">Login</button>}
-            
         </Form>
     );
 }
