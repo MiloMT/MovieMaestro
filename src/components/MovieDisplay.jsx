514a5220c5179e516ddb24ef5e93f3c153a94339
MiloMT <myles@mylestaylor.dev>
2024-02-21 13:23:40 +1000

    Fixing functionality for other movies with clickable cards

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -2,25 +2,27 @@ import React, { useState, useEffect } from "react";
 import OtherMovies from "./OtherMovies.jsx";
 
 const MovieDisplay = ({ movies, genreList }) => {
-  // const [selectedMovieIndex, setSelectedMovieIndex] = useState(null);
-  const [otherMovies, setOtherMovies] = useState([]);
+  const [selectedMovies, setSelectedMovies] = useState([]);
+  const [movieIndex, setMovieIndex] = useState([0])
+  const [isBusy, setBusy] = useState(true)
 
   useEffect(() => {
-    selectOtherMovies();
+    selectMovies();
+    setBusy(false)
   }, []);
 
-  const selectOtherMovies = () => {
+  const selectMovies = () => {
     if (movies && movies.length > 0) {
-      const selectedMovies = [];
-      while (selectedMovies.length < 5) {
+      const movieList = [];
+      while (movieList.length < 5) {
         const randomIndex = Math.floor(Math.random() * movies.length);
-        if (!selectedMovies.includes(movies[randomIndex])) {
-          selectedMovies.push(movies[randomIndex]);
+        if (!movieList.includes(movies[randomIndex])) {
+          movieList.push(movies[randomIndex]);
         }
       }
-      setOtherMovies(selectedMovies);
+      setSelectedMovies(movieList);
     }
-  };
+  }
 
   // useEffect(() => {
   //   if (movies && movies.length > 0 && selectedMovieIndex === null) {
@@ -37,29 +39,35 @@ const MovieDisplay = ({ movies, genreList }) => {
 
   return (
     <>
-      <div>
-        <h2>Movie Display</h2>
-        {otherMovies !== null && movies && movies.length > 0 && (
-          <div className="movie-details">
-            {/* <img
-              src={`https://image.tmdb.org/t/p/original${otherMovies[0].poster_path}`}
-              alt="Movie Poster Image"
-              style={{ width: "300px", height: "400px" }}
-            /> */}
-            <h4>Title: {otherMovies[0].title}</h4>
-            <h6>Overview: {otherMovies[0].overview}</h6>
-            <p>Rating: {otherMovies[0].vote_average}</p>
-            <p>
-              Genres:{" "}
-              {otherMovies[0].genre_ids
-                .map((id) => genreList.find((obj) => obj.id === id).name)
-                .join(", ")}
-            </p>
-          </div>
-        )}
-        {/* <button onClick={selectRandomMovie}>Refresh</button> */}
-      </div>
-      <OtherMovies movies={movies} genreList={genreList} otherMovies={otherMovies} />
+      { isBusy ? (
+        <h1>Loading...</h1>
+      ) : (
+      <>
+        <div>
+          <h2>Movie Display</h2>
+          {selectedMovies !== null && movies && movies.length > 0 && (
+            <div className="movie-details">
+              <img
+                src={`https://image.tmdb.org/t/p/original${selectedMovies[movieIndex].poster_path}`}
+                alt="Movie Poster Image"
+                style={{ width: "300px", height: "400px" }}
+              />
+              <h4>Title: {selectedMovies[movieIndex].title}</h4>
+              <h6>Overview: {selectedMovies[movieIndex].overview}</h6>
+              <p>Rating: {selectedMovies[movieIndex].vote_average}</p>
+              <p>
+                Genres:{" "}
+                {selectedMovies[movieIndex].genre_ids
+                  .map((id) => genreList.find((obj) => obj.id === id).name)
+                  .join(", ")}
+              </p>
+            </div>
+          )}
+          {/* <button onClick={selectRandomMovie}>Refresh</button> */}
+        </div>
+        <OtherMovies genreList={genreList} selectedMovies={selectedMovies} setMovieIndex={setMovieIndex} />
+      </>
+      )}
     </>
   );
 };
--- src/components/OtherMovies.jsx
+++ src/components/OtherMovies.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 
-const OtherMovies = ({ movies, genreList, otherMovies }) => {
-
+const OtherMovies = ({ genreList, selectedMovies, setMovieIndex }) => {
 
   // Function to limit the length of movie overview
   const truncateOverview = (overview, maxLength) => {
@@ -17,27 +16,29 @@ const OtherMovies = ({ movies, genreList, otherMovies }) => {
   return (
     <>
       <CardGroup className="other-movie-cards">
-        {otherMovies.map((movie, index) => (
-          <Card key={index}>
-            <Card.Img
-              variant="top"
-              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-            />
-            <Card.Body>
-              <Card.Title>{movie.title}</Card.Title>
-              <Card.Text>{truncateOverview(movie.overview, 150)}</Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Rating: {movie.vote_average}</small>
-              <br />
-              <small className="text-muted">
-                Genres:{" "}
-                {movie.genre_ids
-                  .map((id) => genreList.find((obj) => obj.id === id).name)
-                  .join(", ")}
-              </small>
-            </Card.Footer>
-          </Card>
+        {selectedMovies.map((movie, index) => (
+          <div key={index} onClick={() => setMovieIndex(index)}>
+            <Card >
+              <Card.Img
+                variant="top"
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              />
+              <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>{truncateOverview(movie.overview, 150)}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Rating: {movie.vote_average}</small>
+                <br />
+                <small className="text-muted">
+                  Genres:{" "}
+                  {movie.genre_ids
+                    .map((id) => genreList.find((obj) => obj.id === id).name)
+                    .join(", ")}
+                </small>
+              </Card.Footer>
+            </Card>
+          </div>
         ))}
       </CardGroup>
     </>
