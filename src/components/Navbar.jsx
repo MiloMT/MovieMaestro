38336f604b8653ee26128fdd54a296d570e46b86
MiloMT <myles@mylestaylor.dev>
2024-02-22 13:22:44 +1000

    Included logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom"
 import { Context } from "./App.jsx"
+import Button from "react-bootstrap/Button"
 
 const NavBar = () => {
     const { api, LoggedIn } = useContext(Context)
     const [isLoggedIn, setLoggedIn] = LoggedIn
+
+    function logout() {
+        setLoggedIn(false)
+        sessionStorage.removeItem("token")
+    }
 
     return (
         <Navbar fixed="top" className="bg-body-tertiary">
@@ -17,9 +23,12 @@ const NavBar = () => {
                 </Link>
                 <h1>MovieMaestro</h1>
                 { isLoggedIn ? (
-                    <Link to="/profile">
-                        <Navbar.Brand>Profile</Navbar.Brand>
-                    </Link>
+                    <>
+                        <Link to="/profile">
+                            <Navbar.Brand>Profile</Navbar.Brand>
+                        </Link>
+                        <Button onClick={logout}>Logout</Button>
+                    </>
                 ) : (
                     <Link to="/login">
                         <Navbar.Brand>login</Navbar.Brand>
