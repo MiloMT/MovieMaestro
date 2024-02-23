5f09436dd52e7e98a9fc691fa19d0ad1169aba3a
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2024-02-23 15:11:21 +1000

    Created SlideFilterMenu and EditProfile

---
+++ src/components/SideFilterMenu.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import FastSearch from './FastSearch';
+import AdvancedSearch from './AdvancedSearch';
+
+function Responsive() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    return (
+        <>
+            <Button  variant="primary" className="d-lg-none" onClick={handleShow}>
+            Search Movie
+            </Button>
+
+            <Offcanvas show={show} onHide={handleClose} responsive="lg">
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Search Movie</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <p className="mb-0">
+                        <FastSearch />
+                        <AdvancedSearch />
+                    </p>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    );
+}
+
+export default Responsive;
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ProfileDetails from '../components/ProfileDetails'
+import EditProfile from '../components/EditProfile.jsx'
 
 
 const Profile = () => {
     return (
         <>
             <ProfileDetails />
+            <EditProfile />
         </>
     )
 }
