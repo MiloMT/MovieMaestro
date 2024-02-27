7ae77dcec3d0dd1cf8a093e5a161ea1dfca0ed4a
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2024-02-27 13:54:06 +1000

    git fetch origin and reset

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -13,6 +13,8 @@ function EditProfile() {
     // Context State
     const { api, LoggedIn, loggedUser, movieList } = useContext(Context)
     const [user, setUser] = loggedUser
+    const [isLoggedIn, setLoggedIn] = LoggedIn
+    const [apiDefaults, setApiDefaults] = api
 
     const [show, setShow] = useState(false)
     const [provider, setProvider] = useState("")
@@ -21,40 +23,59 @@ function EditProfile() {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
 
-    const handleClose = () => setShow(false)
+
+    const handleClose = () => {setShow(false)}
     const handleShow = () => setShow(true)
+
 
     const handleCancel = () => {
         setName("");
         setEmail("");
+        setProvider("")
+        setRegion("")
+        setLanguage("")
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
 
-        const updateEntry = {
-            name: name,
-            email: email,
+        const handleSubmit = async (e) => {
+            e.preventDefault()
+    
+            if (email !=="" || name !=="" || provider !=="" || region !=="" || language !=="") {
+                const updatedEntry = {
+                    name: name == "" ? user.name : name,
+                    email: email == "" ? user.email : email,
+                    streamingPlatform: provider == "" ? user.provider : provider,
+                    region: region == "" ? user.region : region,
+                    language: language == "" ? user.language : language
+                }
+                
+                fetch(`https://moviemaestro-api.onrender.com/users/${user._id}`, {
+                    method: "PATCH",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json; charset=UTF-8",
+                        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+                    },
+                    body: JSON.stringify(updatedEntry),
+                })
+                .then((res) => res.json())
+                .then((data) => setUser(data))
+                .then(() => {
+                    setName("")
+                    setEmail("")
+                    setProvider("")
+                    setRegion("")
+                    setLanguage("")
+                })
+            }  
         }
-        console.log(updateEntry)
-        // id?
-        // const res = await fetch('https://moviemaestro-api.onrender.com/users/?', {
-        //     method: 'PATCH',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(updateEntry)
-        // })
-        // const data = await res.json()
-        // console.log(data)
-    }
+
 
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
                 Edit Profile
             </Button>
-
             <Offcanvas show={show} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
                     <Offcanvas.Title>Edit Profile</Offcanvas.Title>
@@ -64,30 +85,30 @@ function EditProfile() {
                         <h2>Profile Details</h2>
                         <Form.Group as={Row}>
                             <Form.Label htmlFor="nameField">Name</Form.Label>
-                            <Form.Control 
-                                type="name" 
+                            <Form.Control
+                                type="name"
                                 id="nameField"
-                                value={name} 
-                                onChange={e => setName(e.target.value)} 
+                                value={name}
+                                onChange={e => setName(e.target.value)}
                                 placeholder={user.name}
                             />
                         </Form.Group>
                         <Form.Group as={Row}>
                             <Form.Label htmlFor="emailField">Email</Form.Label>
-                            <Form.Control 
-                                type="email" 
+                            <Form.Control
+                                type="email"
                                 id="emailField"
-                                value={email} 
-                                onChange={e => setEmail(e.target.value)} 
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
                                 placeholder={user.email}
                             />
                         </Form.Group>
-                        <ProviderSelector setProvider={setProvider}/>
+                        <ProviderSelector setProvider={setProvider} />
                         <RegionSelector setRegion={setRegion} />
                         <LanguageSelector setLanguage={setLanguage} />
                         <div></div>
                         <Button type="submit" variant="warning">Save</Button>{' '}
-                        <Button type="submit" onClick={handleCancel} variant="secondary">Cancel</Button>{' '}
+                        <Button onClick={handleCancel} variant="secondary">Cancel</Button>{' '}
                     </Form>
                 </Offcanvas.Body>
             </Offcanvas>
