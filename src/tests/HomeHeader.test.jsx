1124dd6a3266277da4ac8db9d498361413154b10
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2024-02-29 08:10:13 +1000

    Tested Home Header

--- src/tests/HomeHeader.test.jsx
+++ src/tests/HomeHeader.test.jsx
@@ -1,28 +1,26 @@
 import '@testing-library/jest-dom/vitest'
 import { React } from 'react'
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { describe, test, expect } from "vitest"
 import  Header  from '../components/HomeHeader'
-import Home from '../views/Home'
-import { Context, FastSearch } from '../components/FastSearch'
+import customRender from './utils/customRender'
+
+const mockContextValue = {
+    api: [[], vi.fn()], // Mock apiDefaults and its setter function
+    LoggedIn: [true, vi.fn()], // Mock isLoggedIn and its setter function
+    loggedUser: ["mockUser", vi.fn()], // Mock user and its setter function
+    movieList: [[], vi.fn()], // Mock movies and its setter function
+    };
 
 //UNIT TESTS FOR HOME HEADER
 describe('Home Header component', () => {
     test('renders the header component', () => {
-        const { container } = render(<Header />)
+        customRender(<Header />, mockContextValue)
         
         // expect(container).toBeInTheDocument()
-        expect(container.querySelector('h1')).toHaveTextContent('Stuck Doom Scrolling?')
+        const HeaderText1 = screen.getByText('Stuck Doom Scrolling?')
+        expect(HeaderText1).toBeInTheDocument()
+        const HeaderText2 = screen.getByText('Let us choose for you!')
+        expect(HeaderText2).toBeInTheDocument()
     })
-})
-
-//INTEGRATION TEST FOR HOME HEADER
-describe('Home integration with Header component', () => {
-test('Header is being rendered within Home.jsx', () => {
-    const { container } = render(<Context.><Home /></FastSearch>)
-
-    // expect(container).toBeDefined(<Header />)
-    const { header } = container.querySelector("test-header")
-    expect(header).toBeInTheDocument()    
-})
 })
