4441a593f3562e9283a18dc8a00b53254ef9d1c8
MiloMT <myles@mylestaylor.dev>
2024-02-28 11:17:04 +1000

    Adjusted formatting for edit profile and profile details components. No longer is a side menu, just a different component on the same screen

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -8,18 +8,21 @@ const ProfileDetails = () => {
     const [user, setUser] = loggedUser
 
     return (
-        <>
-            <h2>Profile Details</h2>
-            <p>Name: {user.name}</p>
-            <p>Email: {user.email}</p>
-            <p>Language: {user.language.label}</p>
-            <p>Region: {user.region.label}</p>
-            <ul>Streaming Providers:
-                {user.streamingPlatform.map((prov) => (
-                <li>{prov.label}</li>
-                ))}
-            </ul>
-        </>
+        <div style={{textAlign: "left"}}>
+            <h1>{user.name}</h1>
+            <h3>{user.email}</h3>
+            <hr />
+            <h4>Streaming Providers</h4>
+            {user.streamingPlatform.map((prov) => (
+                <p>{prov.label}</p>
+            ))}
+            <hr />
+            <h4>Preferred Language</h4>
+            <p>{user.language.label} ({user.language.value})</p>
+            <hr />
+            <h4>Preferred Region</h4>
+            <p>{user.region.label} ({user.region.value})</p>
+        </div>
     )
 }
 
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,12 +1,18 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
+// Bootstrap Components
+import Container from "react-bootstrap/Container"
+import Stack from "react-bootstrap/Stack"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
 // Component Imports
 import ProfileDetails from '../components/ProfileDetails'
 import EditProfile from '../components/EditProfile.jsx'
-import { fetchUserDetails } from "../utils/fetchUserDetails.jsx"
-import { Context } from '../components/App.jsx'
 import WatchedList from '../components/WatchedList.jsx'
 import WishList from '../components/WishList.jsx'
+import { fetchUserDetails } from "../utils/fetchUserDetails.jsx"
+import { Context } from '../components/App.jsx'
 
 
 
@@ -17,6 +23,7 @@ const Profile = () => {
     const [user, setUser] = loggedUser
     // Component State
     const [isBusy, setBusy] = useState(true)
+    const [action, setAction] = useState("View")
     // Object Initialization
     const navigate = useNavigate()
 
@@ -38,12 +45,30 @@ const Profile = () => {
             { isBusy ? (
                 <h1>Loading...</h1>
             ) : (
-                <>
-                    <ProfileDetails />
-                    <EditProfile />
-                    <WatchedList />
-                    <WishList />
-                </>
+                <Container>
+                    <Row>
+                        <Col lg={4}>
+                            <Stack gap={3}>
+                                {action === "View" ? (
+                                    <>
+                                        <ProfileDetails />
+                                        <Button variant="primary" onClick={() => setAction("Edit")}>
+                                            Edit Profile
+                                        </Button>
+                                    </>
+                                ) : (
+                                    <EditProfile setAction={setAction} />
+                                )}
+                            </Stack>  
+                        </Col>
+                        <Col lg={7}>
+                            <Stack gap={3}>
+                                <WatchedList />
+                                <WishList />
+                            </Stack>
+                        </Col>
+                    </Row>
+                </Container>
             )}
         </>
     )
