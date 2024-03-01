3d59f0fa193764f7f73b8d23a5e07b14aae8eaf3
YoshihiroNak <nakagawasaiyoshihiro@gmail.com>
2024-03-01 11:48:51 +1000

    Testing EditProfile

---
+++ src/tests/EditProfile.test.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { screen, fireEvent } from "@testing-library/react";
+import customRender from "./utils/customRender";
+import { vi } from "vitest";
+import EditProfile from "../components/EditProfile";
+
+const mockContextValue = {
+    api: [[], vi.fn()], // Mock apiDefaults and its setter function
+    LoggedIn: [false, vi.fn()], // Mock isLoggedIn and its setter function
+    loggedUser: ["mockUser", vi.fn()], // Mock user and its setter function
+    movieList: [[], vi.fn()], // Mock movies and its setter function
+};
+
+describe("EditProfile component unit tests", () => {
+    test("EditProfile shows a Edit Profile button", () => {
+        customRender(<EditProfile />, mockContextValue)
+    })
+})
