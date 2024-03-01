616cf2323126f1f75e24a155b93443199a96de80
Mfern10 <mitchellfernandez147@gmail.com>
2024-03-01 11:14:41 +0900

    App test complete

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { screen } from "@testing-library/react";
+import customRender from "./utils/customRender";
+import { App } from "../components/App";
+
+
+describe("App unit Tests", () => {
+  test("renders loading message initially", () => {
+    customRender(<App />);
+
+    const loadingMessage = screen.getByText("Loading...");
+    expect(loadingMessage).toBeDefined();
+  });
+});
+
+// without mocking defaults this test successfully checked that App is rendering successfully
