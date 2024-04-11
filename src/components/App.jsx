bd714270bc2ba0b64d2a753e3236a3f6dc194837
MiloMT <myles@mylestaylor.dev>
2024-04-11 10:00:54 +1000

    Adjusted main loading screen with a spinner, extra text and formatting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,10 @@ import Profile from "../views/Profile"
 import NavBar from "./Navbar"
 import Footer from "./Footer"
 import { fetchDefaults } from "../utils/fetchDefaults"
+// Bootstrap Imports
+import Stack from "react-bootstrap/Stack"
+// React-Spinners Imports
+import SyncLoader from "react-spinners/SyncLoader"
 
 
 const Context = React.createContext()
@@ -34,7 +38,16 @@ function App() {
       <>
         {/* During initial defaults fetch, will display loading screen */}
         {isBusy ? (
-          <h1>Loading...</h1>
+          <Stack gap={3} className="loading-container">
+            <h1 className="loading-header">Loading...</h1>
+            <h6>You'll be able to search for movies very shortly!</h6>
+            <SyncLoader 
+                color="#0d6efd" 
+                size={10}
+                aria-label="Loading Spinner"
+                speedMultiplier={0.5}    
+            />
+          </Stack>
         ) : (
           // Context passed through to entire app
           <Context.Provider
